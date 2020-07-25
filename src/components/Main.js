612434dcb8b81665c0b2eb952fa0810db007e0f6
David Trussler <mail@davidtrussler.net>
2020-07-25 15:00:40 +0100

    Create basic components for initial state

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Main() {
+	return (
+	  <main>
+	    <div className="constrained">
+	      <div className="main__inner">
+	        <p>Some instructions</p>
+	      </div>
+	    </div>
+	  </main>
+  )
+}
