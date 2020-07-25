4460cc28f73c864639771516c949cafa5128317f
David Trussler <mail@davidtrussler.net>
2020-07-25 15:04:17 +0100

    Tidies indents

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export function Main() {
 	return (
-	  <main>
-	    <div className="constrained">
-	      <div className="main__inner">
-	        <p>Some instructions</p>
-	      </div>
-	    </div>
-	  </main>
-  )
+		<main>
+			<div className="constrained">
+				<div className="main__inner">
+					<p>Some instructions</p>
+				</div>
+			</div>
+		</main>
+	)
 }
